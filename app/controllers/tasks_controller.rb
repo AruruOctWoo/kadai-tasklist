819aae7adfeee5da1e5bc10f1f4c73333b55b88e
Oct <f17K6067@kanto-gakuin.ac.jp>
2020-04-19 11:28:48 +0000

    fix1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in, only: [:index]
-    before_action :correct_user, only: [:destroy, :update]
+    before_action :correct_user, only: [:destroy, :update, :show]
     
     def index
         @tasks = Task.all
