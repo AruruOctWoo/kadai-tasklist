ff802fbe757b466fcd6374ea19fd256b8e50dbd8
Oct <f17K6067@kanto-gakuin.ac.jp>
2020-04-19 04:34:53 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index]
+  def index
+  end
+
   def new
-      @user = User.new
+    @user = User.new
   end
 
   def create
-      @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to users_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
